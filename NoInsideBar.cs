a410477c9f76f0fba8ee4a367ff7a72586c0f2af
Miroslav Popov <info@forexsb.com>
2012-12-17 17:27:36 +0200

    Added No Inside Bar Indicator

---
+++ NoInsideBar.cs
@@ -0,0 +1,105 @@
+// No Inside Bar Indicator
+// Part of Forex Strategy Builder & Forex Strategy Trader
+// Website http://forexsb.com/
+// Copyright (c) 2006 - 2012 Miroslav Popov - All rights reserved.
+// This code or any part of it cannot be used in other applications without a permission.
+
+using System;
+
+namespace Forex_Strategy_Builder
+{
+    /// <summary>
+    /// Inside Bar Indicator
+    /// </summary>
+    public class NoInsideBar : Indicator
+    {
+        /// <summary>
+        /// Sets the default indicator parameters for the designated slot type
+        /// </summary>
+        public NoInsideBar(SlotTypes slotType)
+        {
+            // General properties
+            IndicatorName = "No Inside Bar";
+            PossibleSlots = SlotTypes.OpenFilter;
+			CustomIndicator = true;
+
+            // Setting up the indicator parameters
+            IndParam = new IndicatorParam();
+            IndParam.IndicatorName = IndicatorName;
+            IndParam.SlotType      = slotType;
+            IndParam.IndicatorType = TypeOfIndicator.Additional;
+
+            // The ComboBox parameters
+            IndParam.ListParam[0].Caption  = "Logic";
+            IndParam.ListParam[0].ItemList = new string[]
+            {
+                "There is no an Inside Bar formation",
+            };
+            IndParam.ListParam[0].Index   = 0;
+            IndParam.ListParam[0].Text    = IndParam.ListParam[0].ItemList[IndParam.ListParam[0].Index];
+            IndParam.ListParam[0].Enabled = true;
+            IndParam.ListParam[0].ToolTip = "Indicator's logic.";
+
+            // The CheckBox parameters
+            IndParam.CheckParam[0].Caption = "Use previous bar value";
+            IndParam.CheckParam[0].Checked = PrepareUsePrevBarValueCheckBox(slotType);
+            IndParam.CheckParam[0].Enabled = true;
+            IndParam.CheckParam[0].ToolTip = "Use the indicator value from the previous bar.";
+
+            return;
+        }
+
+        /// <summary>
+        /// Calculates the indicator's components
+        /// </summary>
+        public override void Calculate(SlotTypes slotType)
+        {
+            // Calculation
+            int iFirstBar = 2;
+            double[] adIB = new double[Bars];
+
+            for (int iBar = 2; iBar < Bars; iBar++)
+            {
+                adIB[iBar] = ((High[iBar - 1] < High[iBar - 2]) && (Low[iBar - 1] > Low[iBar - 2])) ? 0 : 1;
+            }
+
+            // Saving the components
+            Component = new IndicatorComp[2];
+
+            Component[0] = new IndicatorComp();
+            Component[0].CompName  = "Allow long entry";
+            Component[0].DataType  = IndComponentType.AllowOpenLong;
+            Component[0].ChartType = IndChartType.NoChart;
+            Component[0].FirstBar  = iFirstBar;
+            Component[0].Value     = adIB;
+
+            Component[1] = new IndicatorComp();
+            Component[1].CompName  = "Allow short entry";
+            Component[1].DataType  = IndComponentType.AllowOpenShort;
+            Component[1].ChartType = IndChartType.NoChart;
+            Component[1].FirstBar  = iFirstBar;
+            Component[1].Value     = adIB;
+
+            return;
+        }
+
+        /// <summary>
+        /// Sets the indicator logic description
+        /// </summary>
+        public override void SetDescription(SlotTypes slotType)
+        {
+            EntryFilterLongDescription  = "there is no an Inside Bar formation";
+            EntryFilterShortDescription = "there is no an Inside Bar formation";
+
+            return;
+        }
+
+        /// <summary>
+        /// Indicator to string
+        /// </summary>
+        public override string ToString()
+        {
+            return IndicatorName;
+        }
+    }
+}
